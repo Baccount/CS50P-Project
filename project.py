1dda6d2fce2a518d79eaa8afa548fb7c9d4a1aa3
Brandon <jackiam2003@icloud.com>
2022-06-17 14:38:56 -0700

    init after codde transfer

--- project.py
+++ project.py
@@ -1,43 +1,58 @@
+from password_functions import (
+    add_note,
+    clear_screen,
+    delete_database,
+    delete_duplicates,
+    delete_password,
+    find_password,
+    link_account,
+    random_passwords,
+    red,
+    show_passwords,
+    test,
+)
+
+
 def main():
-    print("Hello World")
+    clear_screen()
+    while True:
+        print(
+            "\n\n1: Add password\n2: Display Passwords\n3: Delete Password\n4: Find passwords by name\n5: Test with random password\n6: DELETE DATABASE\n7: Delete duplicate passwords\n8: Add Note\n0: Exit\n"
+        )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        choice = input(red("Choice: "))
+        # add username and password to database
+        if choice == "1":
+            link_account()
+        elif choice == "2":
+            # display all passwords in alphabetical order by username
+            show_passwords()
+        elif choice == "3":
+            # delete password from database using username
+            delete_password()
+        elif choice == "4":
+            # search for password by username
+            find_password()
+        elif choice == "5":
+            # add random passwords to database by number
+            random_passwords()
+        elif choice == "6":
+            # NUKE OPTION delete Entire database
+            delete_database()
+        elif choice == "7":
+            # delete duplicate passwords
+            delete_duplicates()
+        elif choice == "8":
+            # add note to password
+            add_note()
+        elif choice == "0":
+            print("Exiting...")
+            exit(1)
+        elif choice == "test":
+            # developer testing, run all functions
+            test()
+        else:
+            print("\nInvalid choice\n")
 
 
 if __name__ == "__main__":
